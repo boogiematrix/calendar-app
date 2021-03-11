feb81d8a628fa72289a760cf1726d5e04b41303e
Night Chough <chicken.snuggles@gmail.com>
2021-03-11 12:40:07 -0600

    added readme. commented js. added screenshot

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # calendar-app
+
+I was tasked with creating a day planner based on a standard 9-5 business day. It displays the current day and is capable of saving events that persist on page refresh. The planner is divided into hour segments and is color coded to distinguish past, present, and future.
+
+[password-link](https://boogiematrix.github.io/calendar-app/)
+
+![Screen Shot](https://github.com/boogiematrix/calendar-app/blob/main/calendar-screen-shot.png)
--- scheduler.js
+++ scheduler.js
@@ -1,25 +1,11 @@
-/*
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-*/
-
 const currentDay = $("#currentDay");
 const timeBlocks = $(".textarea");
-
+//displays the current day in the header
 currentDay.text(moment().format("dddd MMMM Do"));
+
 let currentHour = moment().format("HH");
 
+//color-codes the textarea based on past, present, and future
 timeBlocks.each(function () {
   if ($(this).attr("data-hour") < currentHour) {
     $(this).removeClass("present");
@@ -30,27 +16,13 @@ timeBlocks.each(function () {
   } else if ($(this).attr("data-hour") > currentHour) {
     $(this).addClass("future");
   }
-
+  //gets saved events from local storage
   $(this).text(localStorage.getItem($(this).attr("data-hour")));
 });
 
-/*for (i = 0; i < timeBlocks.length; i++) {
-  if (timeBlocks[i].dataset.hour < currentHour) {
-    $(timeBlocks[i]).removeClass("present");
-    $(timeBlocks[i]).addClass("past");
-  } else if (timeBlocks[i].dataset.hour == currentHour) {
-    $(timeBlocks[i]).removeClass("future");
-    $(timeBlocks[i]).addClass("present");
-  } else if (timeBlocks[i].dataset.hour > currentHour) {
-    $(timeBlocks[i]).addClass("future");
-  }
-
-  timeBlocks[i].textContent = localStorage.getItem(timeBlocks[i].dataset.hour);
-}*/
-
+//saves text from the textarea in local storage. it is keyed to the hour
 $("button").click(function () {
   let occasion = $(this).prev().val();
   let occasionKey = $(this).prev().attr("data-hour");
-  console.log(occasion);
   localStorage.setItem(occasionKey, occasion);
 });
